ff845082180d308da770524a9b4f13720f14f60d
Stefan Fink <stefan.fink123@gmail.com>
2022-03-09 17:19:26 +0100

    Nortz sajt 1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(window).scroll(function(e) {
+    $('.bg_1').css({transform: 'translateZ(' + window.scrollY / 6 + 'px)'})
+    $('.bg_2').css({transform: 'translateZ(' + window.scrollY / 2 + 'px)',filter: 'blur(' + window.scrollY / 100 + 'px)'})
+    $('.bg_3').css({transform: 'translateZ(' + window.scrollY / 3 + 'px) translateY(' + window.scrollY / 8 + 'px)', filter: 'blur(' + window.scrollY / 400 + 'px)'})
+    $('.bg_4').css({transform: 'translateZ(' + window.scrollY / 6 + 'px)'})
+    $('.bg_5').css({transform: 'translateZ(' + window.scrollY / 4 + 'px)',filter: 'blur(' + window.scrollY / 800 + 'px)'})
+    $('.bg_6').css({transform: 'translateZ(' + window.scrollY / 1 + 'px) translateX(' + window.scrollY / 2 + 'px)'})
+    $('.bg_7').css({transform: 'translateZ(' + window.scrollY / 4 + 'px) translateX(' + -window.scrollY + 'px)'})
+    $('.bg_8').css({transform: 'translateZ(' + window.scrollY / 1 + 'px) translateX(' + window.scrollY / 2 + 'px)'})
+    $('.bg_9').css({transform: 'translateZ(' + window.scrollY / 1 + 'px) translateX(' + window.scrollY / 2 + 'px)'})
+    $('.bg_10').css({transform: 'translateZ(' + window.scrollY / 4 + 'px) translateX(' + window.scrollY / 800+ 'px)'})
+    $('.bg_11').css({transform: 'translateZ(' + window.scrollY / 3 + 'px) translateY(' + window.scrollY / 8 + 'px)', filter: 'blur(' + window.scrollY / 400 + 'px)'})
+
+    if (scrollY > 400) {
+        $('h1').css({transform: 'translateY(' + -window.scrollY / 6 + 'px) translateX(-50%)'})
+     //   $('button').css({transform: 'translateY(' + -window.scrollY / 16 + 'px) translateX(-50%)'})
+    }
+
+})
